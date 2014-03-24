9994420f1374e46fc8cf4300daa442e0b4052740
Ray Zhou <rzhou186@stanford.edu>
2014-03-24 15:23:51 -0700

    Change in naming of file wasn't added by git in last commit

--- js/Tileurl.js
+++ js/Tileurl.js
@@ -2,7 +2,7 @@ function Tileurl(tileurl_id) {
   this.element = $("#" + tileurl_id);
   this.iframe = this.element.find("iframe");
   this.url = this.element.find("input.url");
-  
+
   var that = this;
   this.url.keyup(function() {
     that.update();
@@ -10,7 +10,5 @@ function Tileurl(tileurl_id) {
 }
 
 Tileurl.prototype.update = function() {
-  this.iframe.attr("src", 
-    this.url.val()
-  );
+  this.iframe.attr("src", this.url.val());
 }
