32dde16a3a0238829781fb005c54c326964498f1
Ray Zhou <rzhou186@stanford.edu>
2014-03-24 15:12:47 -0700

    Added real-time TileURL updating

---
+++ js/main.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  new Tileurl("tileurl-left");
+  new Tileurl("tileurl-right");
+}); 
---
+++ js/tileurl.js
@@ -0,0 +1,16 @@
+function Tileurl(tileurl_id) {
+  this.element = $("#" + tileurl_id);
+  this.iframe = this.element.find("iframe");
+  this.url = this.element.find("input.url");
+  
+  var that = this;
+  this.url.keyup(function() {
+    that.update();
+  });
+}
+
+Tileurl.prototype.update = function() {
+  this.iframe.attr("src", 
+    this.url.val()
+  );
+}
